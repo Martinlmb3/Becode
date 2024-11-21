7e460908d7d41868ca1ac197ba3b84dba007376b
Martinlmb3 <martinlumumba1@gmail.com>
2024-11-21 16:38:09 +0100

    Mon premier programme en js

--- variables.js
+++ variables.js
@@ -3,33 +3,38 @@
 // Function 1
 function declareVarOne() {
   // Your code here
-  let myVar = 10
+  const myVar = 10
   return myVar
 }
 
 // Function 2
 function declareVarTwo() {
-  // Your code here
+  const userName = 'John Doe'
+  return userName
 }
 
 // Function 3
 function declareVarThree() {
-  // Your code here
+  const isCompleted = true
+  return isCompleted
 }
 
 // Function 4
 function declareVarFour() {
-  // Your code here
+  const emptyArray = []
+  return emptyArray
 }
 
 // Function 5
 function declareVarFive() {
-  // Your code here
+  const nullVar = null
+  return nullVar
 }
 
 // Function 6
 function declareVarSix() {
-  // Your code here
+  const undefinedVar = undefined
+  return undefined
 }
 
 module.exports = {
