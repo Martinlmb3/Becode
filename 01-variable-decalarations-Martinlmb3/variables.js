c4c06cf067ab05648dc1034317ed1a151b373a58
Martinlmb3 <martinlumumba1@gmail.com>
2025-01-12 18:41:24 +0100

    finish

---
+++ 01-variable-decalarations-Martinlmb3/variables.js
@@ -0,0 +1,47 @@
+// variables.js
+
+// Function 1
+function declareVarOne() {
+  // Your code here
+  const myVar = 10
+  return myVar
+}
+
+// Function 2
+function declareVarTwo() {
+  const userName = 'John Doe'
+  return userName
+}
+
+// Function 3
+function declareVarThree() {
+  const isCompleted = true
+  return isCompleted
+}
+
+// Function 4
+function declareVarFour() {
+  const emptyArray = []
+  return emptyArray
+}
+
+// Function 5
+function declareVarFive() {
+  const nullVar = null
+  return nullVar
+}
+
+// Function 6
+function declareVarSix() {
+  const undefinedVar = undefined
+  return undefined
+}
+
+module.exports = {
+  declareVarOne,
+  declareVarTwo,
+  declareVarThree,
+  declareVarFour,
+  declareVarFive,
+  declareVarSix,
+};
